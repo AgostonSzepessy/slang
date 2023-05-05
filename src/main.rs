d3c725b900d0dc7bc1ad2bbd8dad522b8059aa15
Agoston Szepessy <agoston.the.dev@gmail.com>
2023-05-04 23:50:44 -0700

    Add basic grammar and dump AST of programs
    Grammar can construct expressions, variable assignments, and print
    statements.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use libslang::lexer::Lexer;
+
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar, "/libslang/grammar.rs");
+
+use grammar::ScriptParser;
+
 fn main() {
-    println!("Hello, world!");
-    
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let source_code = std::fs::read_to_string(&args[1]).unwrap();
+        let lexer = Lexer::new(&source_code[..]);
+        let parser = ScriptParser::new();
+        let ast = parser.parse(lexer).unwrap();
+
+        println!("{:?}", ast);
+    } else {
+        println!("no source file provided");
+    }
 }
